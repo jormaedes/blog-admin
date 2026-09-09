b4908799c94cb7785ef3e51423266df8750a98b6
Jormaedes Luís <jormahed@gmail.com>
2026-09-09 12:38:36 +0100

    feat: add Header component and integrate it into the layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+	return (
+		<header className="">
+			<div className="flex items-center justify-between py-2 container mx-auto">
+				<div>Blog Admin</div>
+				<button>Login</button>
+			</div>
+		</header>
+	);
+}
