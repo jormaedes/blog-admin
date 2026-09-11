3203df2766b4ba05b96ed0e4c457107b66f89587
Jormaedes Luís <jormahed@gmail.com>
2026-09-11 18:34:00 +0100

    feat(profile): display current user information

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import useAuthStore from "@/stores/authStore";
+
 export default function ProfilePage() {
+  const user = useAuthStore((state) => state.user);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <main>
       <h1>Perfil</h1>
+
+      <div>
+        <p>
+          <strong>Nome:</strong> {user.firstName}
+        </p>
+
+        <p>
+          <strong>Apelido:</strong> {user.lastName}
+        </p>
+
+        <p>
+          <strong>Username:</strong> {user.username}
+        </p>
+
+        <p>
+          <strong>Tipo:</strong> {user.userType}
+        </p>
+      </div>
     </main>
   );
 }
