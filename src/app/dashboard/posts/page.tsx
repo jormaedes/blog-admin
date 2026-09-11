12acf8ad3aa87a225ea6e9a9a6e8610e31e586f4
Jormaedes Luís <jormahed@gmail.com>
2026-09-11 12:06:36 +0100

    feat: add data format and buttons to edit, pulblish and delete a post

--- src/app/dashboard/posts/page.tsx
+++ src/app/dashboard/posts/page.tsx
@@ -4,11 +4,16 @@ import { useEffect, useState } from "react";
 
 import { getPosts, getToken } from "@/lib/api";
 import type { Post } from "@/types/post";
+import Link from "next/link";
 
 export default function PostsPage() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
+
+  function formatDate(timestamp: string) {
+    return new Date(timestamp).toLocaleDateString("pt-PT");
+  }
 
   useEffect(() => {
     async function loadPosts() {
@@ -49,7 +54,28 @@ export default function PostsPage() {
       {posts.map((post) => (
         <article key={post.id}>
           <h2>{post.title}</h2>
+
+          <p>{formatDate(post.timestamp)}</p>
+
+          <p>
+            {post.published ? "Publicado" : "Rascunho"}
+          </p>
+
           <p>{post.content}</p>
+
+          <div>
+            <Link href={`/dashboard/posts/${post.id}/edit`}>
+              Editar
+            </Link>
+
+            <button type="button">
+              {post.published ? "Despublicar" : "Publicar"}
+            </button>
+
+            <button type="button">
+              Apagar
+            </button>
+          </div>
         </article>
       ))}
     </main>
